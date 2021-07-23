139734e1314b33a3cdcc46b161bfdf7d9de61507
MixLab-Tech <chizhiwei007@163.com>
2021-07-23 19:54:32 +0800

    update

--- src/lab/base.js
+++ src/lab/base.js
@@ -21,12 +21,13 @@ class Base {
     sleep = m => new Promise(r => setTimeout(r, m));
 
     shuffle(arr) {
+        let arrNew=[...arr];
         //用Math.random()函数生成0~1之间的随机数与0.5比较，返回-1或1
         const randomsort = function(a, b) {
                 return Math.random() > .5 ? -1 : 1;
             }
             // var arr = [1, 2, 3, 4, 5];
-        return arr.sort(randomsort);
+        return arrNew.sort(randomsort);
     }
 
 
